bc63298376419f24a2b130da711950b8b0495400
Timiemmy <faith.bolanle@yahoo.com>
2023-03-26 16:08:46 +0100

    added signup and login views and forms

--- store/settings.py
+++ store/settings.py
@@ -123,6 +123,10 @@ MEDIA_URL = "media/"
 # this will create media folder in the root folder of the project
 MEDIA_ROOT = BASE_DIR / "media"
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
